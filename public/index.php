f19159b9e96ffab7688925f70422dd40144d5b81
seejansunar <czansunar0098@gmail.com>
2022-07-02 23:04:26 +0545

    separated URL elements

--- public/index.php
+++ public/index.php
@@ -2,10 +2,20 @@
 
     class App
     {
+        function __construct()
+        {
+            print_r($this->getURL());
+        }
 
+        private function getURL()
+        {
+            $url = $_GET['url'] ?? 'home';
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $arr = explode('/', $url);
+            return $arr;
+        }
     }
 
     $app = new App();
-    var_dump($app);
 
 ?>
